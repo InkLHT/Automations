9ccd747d1cd6b2304a8cb215fdcf7d97986fa684
Manel <116504013+InkLHT@users.noreply.github.com>
2024-10-16 23:21:26 +0200

    Update Desactivation.bat

--- Desactivation.bat
+++ Desactivation.bat
@@ -4,7 +4,7 @@ chcp 65001 >nul
 echo Désactivation de l'automatisation car je ne sais faire que ça...
 
 rem Lire le PID du fichier dans le dossier logs
-set /p PID=<C:\Users\Manel\OneDrive\Bureau\Automatisation\Logs_PID\Surveillance-docs.txt
+set /p PID=<C:\Users\CHEMIN_VERS\VOTRE_DOSSIER_PID
 
 rem Tuer le processus en utilisant le PID
 taskkill /F /PID %PID%
