be9f8144fdea0154e7c14200d963f186d08183c8
Manel <116504013+InkLHT@users.noreply.github.com>
2024-10-16 23:20:41 +0200

    Update Automatisation.bat

--- Automatisation.bat
+++ Automatisation.bat
@@ -1,8 +1,7 @@
 @echo off
-rem Les commentaires doivent être sur une seule ligne !
 rem Change l'encodage du terminal à UTF-8
 chcp 65001 >nul  
 echo Démarrage de la surveillance...
-start /B python C:\Users\Manel\OneDrive\Bureau\Automatisation\Scripts_Python\Surveillance-docs.py
+start /B python C:\Users\CHEMIN_VERS\VOTRE_SCRIPT
 echo Surveillance démarrée.
 pause
